cf17e6532e161dcc041b22cec137b427298ae3f4
Simone Roveri <spikeroveri91@gmail.com>
2024-02-29 08:17:37 +0900

    fixed indentation]

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I was change on feature branch!
-        </a>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> I was change on feature branch! </a>
       </header>
     </div>
   );
